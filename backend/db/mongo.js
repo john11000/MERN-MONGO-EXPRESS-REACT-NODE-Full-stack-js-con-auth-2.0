4449e0e2fdbc7662d172d9371ab6f3243c011a51
Jhon2410 <86936709+Jhon2410@users.noreply.github.com>
2021-11-13 21:40:46 -0500

    Update mongo.js

--- backend/db/mongo.js
+++ backend/db/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect("mongodb+srv://anderson:anderson@cluster0.hykao.mongodb.net/test?retryWrites=true&w=majority");
+  await mongoose.connect("mongodb+srv://<su usuario>:<su contraseña>@cluster0.hykao.mongodb.net/test?retryWrites=true&w=majority");
 }
 module.exports = mongoose;
